9e2d25360ab3d221b65af92b44fbc24d3db4ef49
Simon Steer <simonpsteer@gmail.com>
2022-10-11 17:09:13 -0400

    initial

---
+++ shared/test.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mySharedFunction = void 0;
+var mySharedFunction = function () { return ({ foo: 'bar' }); };
+exports.mySharedFunction = mySharedFunction;
